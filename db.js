50422bcdce26ac1645eb31baa4096ad05e77dc82
Leela Purnima <purnima.tatina24@sasi.ac.in>
2026-07-13 15:30:47 +0530

    Update db.js

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const dns=require("dns")
 dns.setServers(["8.8.8.8","8.8.4.4"])
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://purnimatatina24_db_user:Leela@19@cluster0.zcpo10k.mongodb.net/leela?appName=Cluster0'); 
+    await mongoose.connect('mongodb+srv://purnimatatina_db_user:Leela@19@cluster0.umvpmlg.mongodb.net/'); 
     console.log('MongoDB Connected to studentDB');
   } catch (err) {
     console.error(err);
