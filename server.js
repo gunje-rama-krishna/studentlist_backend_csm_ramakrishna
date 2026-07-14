f058c5dda8af70c260ec1837add5936ed9d99c5f
gunje-rama-krishna <gunjeramakrishna2007@gmail.com>
2026-07-14 13:27:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json()); 
 
-const uri = "mongodb+srv://dvikas254202:vikas2006@cluster0.irtwbpd.mongodb.net/vikas?appName=Cluster0";
+const uri = "mongodb+srv://gunjeramakrishna:rama1234krishna@cluster0.jmyge10.mongodb.net/ramakrishna?appName=Cluster0";
 const client = new MongoClient(uri);
 let studentsCollection;
 
